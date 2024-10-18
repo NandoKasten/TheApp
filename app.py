f49dd5c9f04615036087c06879e71cb7c464de0a
Nando <nando@nktech.de>
2024-10-18 15:46:23 +0200

    push

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ def logout():
 login_page = st.Page(login, title="Log in", icon=":material/login:")
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 
-mapping_demo = st.Page("pages/test/101_Mapping_Demo.py", title="Mapping Demo", icon=":material/dashboard:")
-plotting_demo = st.Page("pages/test/102_Plotting_Demo.py", title="Plotting Demo", icon=":material/dashboard:")
-dataframe_demo = st.Page("pages/test/103_DataFrame_Demo.py", title="Data Frame Demo", icon=":material/bug_report:")
+mapping_demo = st.Page("views/test/101_Mapping_Demo.py", title="Mapping Demo", icon=":material/dashboard:")
+plotting_demo = st.Page("views/test/102_Plotting_Demo.py", title="Plotting Demo", icon=":material/dashboard:")
+dataframe_demo = st.Page("views/test/103_DataFrame_Demo.py", title="Data Frame Demo", icon=":material/bug_report:")
 
 
-settings = st.Page("pages/settings/99_Settings.py", title="Settings", icon=":material/settings:")
+settings = st.Page("views/settings/99_Settings.py", title="Settings", icon=":material/settings:")
 
 if st.session_state.logged_in:
     pg = st.navigation(
